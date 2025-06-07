db348390376855343e6a9ae444bebf0f96dfaac7
aaryaman22 <aaryaman1436.be20@chitkara.edu.in>
2025-06-08 00:20:54 +0530

    p-8 addiing kafka for notification

--- connections-service/src/main/java/com/project/linkedin/connections_service/controller/ConnectionsController.java
+++ connections-service/src/main/java/com/project/linkedin/connections_service/controller/ConnectionsController.java
@@ -4,10 +4,7 @@ import com.project.linkedin.connections_service.entity.Person;
 import com.project.linkedin.connections_service.service.ConnectionsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,20 @@ public class ConnectionsController {
     @GetMapping("/first-degree")
     public ResponseEntity<List<Person>> getFirstConnections() {
         return ResponseEntity.ok(connectionsService.getFirstDegreeConnections());
+    }
+
+    @PostMapping("/request/{userId}")
+    public ResponseEntity<Boolean> sendConnectionRequest(@PathVariable Long userId) {
+        return ResponseEntity.ok(connectionsService.sendConnectionRequest(userId));
+    }
+
+    @PostMapping("/accept/{userId}")
+    public ResponseEntity<Boolean> acceptConnectionRequest(@PathVariable Long userId) {
+        return ResponseEntity.ok(connectionsService.acceptConnectionRequest(userId));
+    }
+
+    @PostMapping("/reject/{userId}")
+    public ResponseEntity<Boolean> rejectConnectionRequest(@PathVariable Long userId) {
+        return ResponseEntity.ok(connectionsService.rejectConnectionRequest(userId));
     }
 }
--- connections-service/src/main/java/com/project/linkedin/connections_service/entity/Person.java
+++ connections-service/src/main/java/com/project/linkedin/connections_service/entity/Person.java
@@ -17,3 +17,4 @@ public class Person {
 
     private String name;
 }
+
